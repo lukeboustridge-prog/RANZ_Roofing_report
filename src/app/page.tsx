9201dd5e2023b5a26af17b0db5acaf419688beec
Luke Boustridge <luke.boustridge@gmail.com>
2026-01-23 15:23:52 +1300

    debug: Minimal test page to debug 404

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>RANZ Roofing Reports</h1>
+      <p>If you can see this, the app is working.</p>
+    </div>
+  );
+}
